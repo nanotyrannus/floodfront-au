b83b077efb47a94a4f6c56a8a967c5c24e22ebac
Ryan Joseph Constantino <ryan.constantino93@gmail.com>
2017-04-09 19:38:12 -0700

    Begin implementing search interface

--- src/resources/elements/search/search.ts
+++ src/resources/elements/search/search.ts
@@ -1,18 +1,27 @@
 export class SearchCustomElement {
     private top: number
     private display: string
+    private _isActive: boolean
 
     constructor() {
         this.top = 100
         this.display = "none"
+        this._isActive = false
     }
 
     public show() {
         this.top = 0
         this.display = "block"
+        this._isActive = true
     }
     public hide() {
         this.top = 100
         this.display = "none"
+        this._isActive = false
     }
+
+    get isActive(): boolean {
+        return this._isActive
+    }
+
 }
