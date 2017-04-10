6753a5815017a772c60f0847ee29055eff5f4677
Ryan Joseph Constantino <ryan.constantino93@gmail.com>
2017-04-09 20:50:20 -0700

    Change require.js timeout to 999 seconds.

--- src/resources/elements/search/search.ts
+++ src/resources/elements/search/search.ts
@@ -1,12 +1,20 @@
+import { autoinject } from "aurelia-framework"
+import { SearchService } from "../../../search-service/search-service"
+
 export class SearchCustomElement {
     private top: number
     private display: string
     private _isActive: boolean
+    private locations: any[]
 
-    constructor() {
+    constructor(private searchService: SearchService) {
         this.top = 100
         this.display = "none"
         this._isActive = false
+    }
+
+    public async search(query: string) {
+        this.locations = await this.searchService.search(query)
     }
 
     public show() {
